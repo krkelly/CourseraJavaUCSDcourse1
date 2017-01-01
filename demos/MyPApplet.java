6e90167357e2ab1fcc8d8f0ba806c7907c81a167
krkelly <0x6b6576696e@gmail.com>
2016-12-31 18:55:31 -0700

    modified MyPApplet to see how the lecture code executes

--- demos/MyPApplet.java
+++ demos/MyPApplet.java
@@ -12,23 +12,26 @@ import processing.core.PImage;
  *
  */
 public class MyPApplet extends PApplet{
-	PImage img;
+//	PImage img;
+        private String URL = "http://...jpg";
+        private PImage backgroundImg;
 	
 	public void setup() {
 		//Add setup code for MyPApplet
 		size(400,400);				//set canvas size
-		background(255);			//set canvas color
-		stroke(0);				//set pen color
-		img = loadImage("palmTrees.jpg", "jpg");
+		backgroundImg = loadImage(URL,"jpg");
+//		background(255);			//set canvas color
+//		stroke(0);				//set pen color
+//		img = loadImage("palmTrees.jpg", "jpg");
 	}
 	
 	public void draw() {
 		//Add drawing code for MyPApplet
-		img.resize(0, height);			//resize loaded image to full height of canvas
-		image(img, 0, 0);			//display image 
-		int[] color = sunColorSec(second());		//calculate color code for sun
-		fill(color[0],color[1],color[2]);	//set sun color
-		ellipse(width/4,height/5,width/4,height/5);	//draw sun
+//		img.resize(0, height);			//resize loaded image to full height of canvas
+//		image(img, 0, 0);			//display image 
+//		int[] color = sunColorSec(second());		//calculate color code for sun
+//		fill(color[0],color[1],color[2]);	//set sun color
+//		ellipse(width/4,height/5,width/4,height/5);	//draw sun
 		
 	}
 	
