4fd4c2a3f7fc65f92fd927d2bc5521467b588e80
krkelly <0x6b6576696e@gmail.com>
2017-01-25 17:46:39 -0700

    Removed redundant code from OceanQuake and LandQuake and put in EarthquakeMarker

--- module6/OceanQuakeMarker.java
+++ module6/OceanQuakeMarker.java
@@ -32,20 +32,4 @@ public class OceanQuakeMarker extends EarthquakeMarker {
 	}
 
 
-    @Override
-    public int compareTo(EarthquakeMarker o) {
-        if (this.getMagnitude() > o.getMagnitude())
-        {
-            return 1;
-        }
-        else if (this.getMagnitude() > o.getMagnitude())
-        {
-            return -1;
-        }
-        return 0;
-    }
-	
-
-	
-
 }
