15dd5d0fe39dcaa282af6b419b9c78dde4d65161
krkelly <0x6b6576696e@gmail.com>
2017-01-29 20:33:47 -0700

    added basic code to mark colorado state parks on map

--- module6/EarthquakeMarker.java
+++ module6/EarthquakeMarker.java
@@ -183,11 +183,11 @@ public abstract class EarthquakeMarker extends CommonMarker implements Comparabl
 	public int compareTo(EarthquakeMarker o) {
 	    if (this.getMagnitude() > o.getMagnitude())
 	    {
-	        return 1;
+	        return -1;
 	    }
 	    else if (this.getMagnitude() < o.getMagnitude())
 	    {
-	        return -1;
+	        return 1;
 	    }
 	    return 0;
 	}
